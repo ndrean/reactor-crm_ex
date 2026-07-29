44b50d787ffc9c9a52d3b677d20a35fe8c50e347
ndrean <nevendrean@yahoo.fr>
2026-07-29 11:33:07 +0200

    switch to Postgres/Pg_vector image & migration create pg_vector

--- priv/repo/migrations/20260729092404_create_vector_extension.exs
+++ priv/repo/migrations/20260729092404_create_vector_extension.exs
@@ -2,6 +2,6 @@ defmodule CrmReactor.Repo.Migrations.CreateVectorExtension do
   use Ecto.Migration
 
   def change do
-
+    execute "CREATE EXTENSION IF NOT EXISTS vector", "DROP EXTENSION IF EXISTS vector"
   end
 end
